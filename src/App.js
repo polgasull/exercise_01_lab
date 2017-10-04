e1912a097e19e2ff31bf3193f416f12edc135c4d
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2017-10-04 17:58:52 +0200

    Comment line Card component Add Instructions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-import Card from './components/Card';
+// import Card from './components/Card';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const deleteThisLine = <p>Create Card Component</p>
     return (
       <div className="container">
-        <Card />
+        {deleteThisLine}
+        {/* <Card /> */}
       </div>
     );
   }
